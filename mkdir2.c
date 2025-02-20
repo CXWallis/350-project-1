ec8e42b64b33075fcf5cdd8585f2d8e255fb8d54
xavier wallis <wallisxdevelopment@gmail.com>
2025-02-20 14:02:59 -0500

    added mkdir2 functionality

---
+++ mkdir2.c
@@ -0,0 +1,35 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+/* ATTENTION: to ensure correct compilation of the base code, 
+   stub functions for the system call user space wrapper functions are provided. 
+   REMEMBER to disable the stub functions (by commenting the following macro) to 
+   allow your implementation to work properly. */
+
+#define STUB_FUNCS
+#ifdef STUB_FUNCS
+void mkdir2(char * d1, char* d2) {
+  const int first = mkdir( d1 );
+  const int second = mkdir( d2 );
+
+  if ( first | second < 0 ) printf( 2, "mkdir failed to create <%s> and <%s>\n", d1, d2 );
+  exit();
+}
+#endif
+
+
+/* IMPORTANT INSTRUCTION: the code below should not be changed. 
+    Failure to follow this instruction will lead to zero point 
+	for this part */
+
+int 
+main(int argc, char * argv[])
+{    
+    if(argc != 3){
+        printf(1, "Please provid the names of two directories to create");
+    }
+
+	mkdir2(argv[1], argv[2]);
+    exit(); //return 0;
+}
